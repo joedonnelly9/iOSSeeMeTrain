49e51b35409ffc12d45ef77cdf249970a80931ba
Vagrant <vagrant@vagrant-osx.local>
2016-04-13 19:53:40 -0700

    Initial Commit

---
+++ SeeMeTrain/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SeeMeTrain
+//
+//  Created by Vagrant on 4/13/16.
+//  Copyright (c) 2016 Sapphire. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
