1c237f8517d4588b742eab4c62a9206bac2eb784
gad <gad@mac-cl1174.cse.ohio-state.edu>
2016-04-18 17:20:30 -0400

    about added

--- SeeMeTrain/SecondViewController.swift
+++ SeeMeTrain/SecondViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    @IBOutlet weak var schedule: UISlider!
+    @IBOutlet weak var alarmStatus: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
 }
 
